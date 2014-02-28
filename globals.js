e70bbd9dfc6fa53266fc443fd59a2caacfe83faa
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-02-28 00:30:16 -0800

    update notes

--- globals.js
+++ globals.js
@@ -1,18 +1,18 @@
 /// Globals
-var y = 2;
+  var y = 2;
 
   // *Antipattern*: var literal forgotten
-x = 1;
+  x = 1;
 
   // Every global variable you create becomes a property of the global object 
   // in the global excecution context ' this ' refers to the global object 
-this.x;    // => 1 
-this['x']; // => 1
+  this.x;    // => 1 
+  this['x']; // => 1
 
   // In browsers there's another reference called ' window ' which points to the global object itself 
-this === window; // => true
-window.y;        // => 2
-window['y'];     // => 2
+  this === window; // => true
+  window.y;        // => 2
+  window['y'];     // => 2
 
   // Globals are shared in the same global namespace , this is a problem because 
   // there's a chance of overriding values 
@@ -20,43 +20,41 @@ window['y'];     // => 2
   // Implied Globals
   // Every variable you don't declare becomes a property of the global object
   // Example : 
-function secretkey() {
-  password = "abc123"; // *Antipattern*
-  return md5(password);
-}
-secretkey(); // => e99a18c428cb38d5f260853678922e03
-password;    // => "abc123"
+  function secretkey() {
+    password = "abc123"; // *Antipattern*
+    return md5(password);
+  }
+  secretkey(); // => e99a18c428cb38d5f260853678922e03
+  password;    // => "abc123"
 
 /// Chained Assignments in the declaration create implied globals 
-function fn() {
-  var x = y = z = 0; // *Antipattern*
-}
+  function fn() {
+    var x = y = z = 0; // *Antipattern*
+  }
   // there's a right-left evaluation 
   // where the expression ' z = 0 ' is evaluated first , so ' z ' is not declared neither ' y '
   // ' x ' is the only declared variable
-fn();
-z;    // => 0
-y;    // => 0
-x;    // => "Error: x is not defined"
+  fn();
+  z;    // => 0
+  y;    // => 0
+  x;    // => "Error: x is not defined"
 
   // Is valid after the declaration 
-function fn() {
-  var x, y, z;
-  x = y = z = 0; // Valid pattern
-}
+  function fn() {
+    var x, y, z;
+    x = y = z = 0; // Valid pattern
+  }
 
   // implied globals can be deleted , global declared variables can't 
-var x = 1; 
-y = 2; 
+  var x = 1; 
+  y = 2; 
 
-delete x; // => false
-x;        // => 1
-delete y; // => true
-y;        // => "Error: y is not defined"
+  delete x; // => false
+  x;        // => 1
+  delete y; // => true
+  y;        // => "Error: y is not defined"
 
 /// Accesing Global Object without hardcoding ' window ' (doesn't work with strict mode)
-var global = (function () {
-  return this;
-}()); 
-
-
+  var global = (function () {
+    return this;
+  }());
--- variables.js
+++ variables.js
@@ -1,29 +1,29 @@
 /// Single var pattern 
   // Common way to declare a group of variables  
-function fn() {
-  var start = 0;
-  var finish = 1;
-  var counter = 0;
-  var increase = 2.5;
-  var result; 
-}
+  function fn() {
+    var start = 0;
+    var finish = 1;
+    var counter = 0;
+    var increase = 2.5;
+    var result; 
+  }
 
   // Single ' var ' Pattern*
-function fn() {
-  var start,
-      finish = 1,
-      counter = 0,
-      increase = 2.5,
-      result; 
-}
+  function fn() {
+    var start,
+        finish = 1,
+        counter = 0,
+        increase = 2.5,
+        result; 
+  }
 
   // Some programmers arrange it like this 
-function fn() {
-  var start
-  , finish = 1
-  , counter = 0
-  , increase = 2.5
-  , result; 
-}
+  function fn() {
+    var start
+    , finish = 1
+    , counter = 0
+    , increase = 2.5
+    , result; 
+  }
 
 /// Hoisting
