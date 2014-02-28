0121a20eccede255a6d43f76e3a0894e5d32916b
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-02-27 18:57:48 -0800

    updating globals

--- globals.js
+++ globals.js
@@ -1,7 +1,7 @@
 // Globals
 var y = 2;
 
-// Antipattern: var literal forgotten
+// *Antipattern*: var literal forgotten
 x = 1;
 
 // Every global variable you create becomes a property of the global object 
@@ -14,3 +14,15 @@ this === window; // => true
 window.y;        // => 2
 window['y'];     // => 2
 
+// Globals are shared in the same global namespace , this is a problem because 
+// there's a chance of overriding values 
+
+// Implied Globals
+// Every variable you don't declare becomes a property of the global object
+// Example : 
+function secretkey() {
+  password = "abc123"; // *Antipattern*
+  return md5(password);
+}
+secretkey(); // => e99a18c428cb38d5f260853678922e03
+password;    // => "abc123"
