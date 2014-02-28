c731b44fef5a80989ff2aae84d3c55bcb07341c5
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-02-27 20:16:37 -0800

    update notes

--- globals.js
+++ globals.js
@@ -5,11 +5,11 @@ var y = 2;
 x = 1;
 
 // Every global variable you create becomes a property of the global object 
-// in the global excecution context 'this' refers to the global object 
+// in the global excecution context ' this ' refers to the global object 
 this.x;    // => 1 
 this['x']; // => 1
 
-// In browsers there's another reference called 'window' which points to the global object itself 
+// In browsers there's another reference called ' window ' which points to the global object itself 
 this === window; // => true
 window.y;        // => 2
 window['y'];     // => 2
@@ -26,3 +26,31 @@ function secretkey() {
 }
 secretkey(); // => e99a18c428cb38d5f260853678922e03
 password;    // => "abc123"
+
+// Chained Assignments create implied globals 
+function fn() {
+  var x = y = z = 0; // *Antipattern*
+}
+// there's a right-left evaluation 
+// where the expression ' z = 0 ' is evaluated first , so ' z ' is not declared neither ' y '
+// ' x ' is the only declared variable
+fn();
+z;    // => 0
+y;    // => 0
+x;    // => "Error: x is not defined"
+
+// implied globals can be deleted , global declared variables can't 
+var x = 1; 
+y = 2; 
+
+delete x; // => false
+x;        // => 1
+delete y; // => true
+y;        // => "Error: y is not defined"
+
+//Accesing Global Object without hardcoding ' window ' (doesn't work with strict mode)
+var global = (function () {
+  return this;
+}()); 
+
+
