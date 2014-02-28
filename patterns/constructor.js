aac1327d2746e7f40d9e7d377420c8838aea4045
Alejandro Amador <ing.ramon.alejandro@gmail.com>
2014-02-28 12:49:29 -0800

    update notes 0

---
+++ patterns/constructor.js
@@ -0,0 +1,5 @@
+/// Constructor Pattern 
+  // Ways to create an Object
+  var obj = {};                               // Short 
+  var obj = new Object();                     // Using the built-in 
+  var obj = Object.create(Object.prototype);  // Method Creates an object with a specified prototype 
