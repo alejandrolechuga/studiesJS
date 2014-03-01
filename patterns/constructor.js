7724cacc71ca20c96b93d548a1d6bd3c1e308a8e
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-02-28 16:01:52 -0800

    update notes

--- patterns/constructor.js
+++ patterns/constructor.js
@@ -3,3 +3,31 @@
   var obj = {};                               // Short 
   var obj = new Object();                     // Using the built-in 
   var obj = Object.create(Object.prototype);  // Method Creates an object with a specified prototype 
+  
+  // Ways to assign keys to an object
+  obj.key = "value";
+  obj.key;              // => "value"
+  obj["key"] = "value";
+  obj["key"]            // => "value"
+
+  //ES5 Approaches
+  //Object.defineProperty
+  Object.defineProperty(obj, "key", {
+    value: "value",
+    writable: true,
+    enumerable: true,
+    configurable: true
+  });
+
+  //defineProperty Short-hand 
+  var defineProp = function (obj, key, value) {
+    var config = {
+      value: value,
+      writable: true,
+      enumerable: true,
+      configurable: true
+    };
+    Object.defineProperty(obj, key, config);
+  };
+
+  
