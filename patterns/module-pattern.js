99bed95f7f336a38e73e46a234081b64bc6f65a2
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-03-01 15:35:18 -0800

    update notes

--- patterns/module-pattern.js
+++ patterns/module-pattern.js
@@ -1,9 +1,12 @@
 /// The Module Pattern
   // Module Pattern is based in part on the Object Literal Pattern 
   // Module Pattern using IIFE (Immediately-Invoked Function Expression)
-
+  // this pattern can emulate private properties using the function scope
   var module = (function(){
+    // privatekey is not exposed , only the public methods have access to it
     var privatekey = "abc123";
+
+    // It returns an object with access to a private properties
     return {
       getKey: function() {
         return privatekey;
@@ -17,3 +20,20 @@
   module.getKey();    // => "abc123";
   module.emptyKey();  
   module.getKey();    // => "";
+
+  // Importing Mixins
+  // You can pass globals through our anonymous function and localy name them as you wish
+  var module = (function (window, $) {
+    var element = $("<input>");
+    // private method
+    function getElement(){
+      return element[0];
+    }
+    return {
+      getEl: function () {
+        // Calling a private method
+        return getElement();
+      }
+    }
+  }(this, jQuery));
+  module.getEl(); // => <input>
