f320ff6bb6e9ca6f0113f48abf522d48b7a6d55c
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-03-05 22:13:26 -0800

    update notes adding eval, Function evaluations

--- language/eval.js
+++ language/eval.js
@@ -15,3 +15,38 @@
     myFunc(1, 2, 3);
   }, 1000);
 
+/// Global object pollution 
+  // examples
+  console.log(typeof uno);      // => undefined 
+  console.log(typeof dos);      // => undefined
+  console.log(typeof tres);     // => undefined
+
+  // messes up with the global object
+  var string = "var uno = 1; console.log(uno);";
+  eval(string);                 // => 1
+  // functions are more sandboxed 
+  var string = "var dos = 2; console.log(dos);";
+  new Function(string);         // => 2
+
+  var string = "var tres = 3; console.log(tres);";
+  (function(){
+    eval(string);
+  }());                         // => 3
+
+  console.log(typeof uno);      // => "number"
+  console.log(typeof dos);      // => undefined
+  console.log(typeof tres);     // => undefined
+/// eval can modify it's outer scope 
+  (function(){
+    // Outer Scope
+    var local = "value";
+    eval("local = false; console.log(local);"); // => false
+    console.log(local);                         // => false
+  }());
+  
+  // in the other hand 
+  (function() {
+    var local = 1; 
+    Function("console.log(typeof local);")(); // => undefined 
+  }());
+
