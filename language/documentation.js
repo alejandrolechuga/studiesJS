98af41bdf3b9a82109ee376d78f5bd0e013645ea
Alejandro Lechuga <ing.ramon.alejandro@gmail.com>
2014-03-05 22:34:17 -0800

    update notes adding eval, Function evaluations

---
+++ language/documentation.js
@@ -0,0 +1,25 @@
+
+/**
+* JavaScript Application Description here
+* @module APP
+*/
+
+var APP = {};
+
+/**
+* Library for Something 
+* @namespace APP
+* @class DoSomething
+*/
+APP.DoSomething = {
+  /**
+  * This method does many things
+  * @method foo
+  * @param   {Number} a first parameter
+  * @param   {Number} b sencond parameter 
+  * @return  {Number} Something
+  */
+  foo: function(a, b){
+    return a + ":" + b;
+  }
+};
