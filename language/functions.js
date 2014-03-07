10c83e27be8a2b2ff8abc1e51990264330d8f7dc
Alejandro Lechuga <alejandrolechuga@Alejandros-MacBook-Pro-2.local>
2014-03-06 23:17:04 -0800

    update notes about functions

---
+++ language/functions.js
@@ -0,0 +1,21 @@
+/// functions 
+  // Antipattern
+  // Passing large number of parameters is not convenient. 
+  addCar("Mustang", "Ford", 2005, "red", new Date(), "California");
+  // Is better to use objects instead
+  addCar({
+      model: "Mustang"
+    , maker: "Ford"
+    , year: 2005
+    , color: "red"
+    , time: new Date()
+    , state: "California"
+  });
+
+  // function application 
+  function fn(a, b) {
+    return a + b;
+  }
+  // Calling with apply
+  // The first parameter is null so this points to the global object
+  fn.apply(null, [1, 2]); // => 3
